e5b1f2914d5e2cd25dde8b1ce5176aab5ec080a2
Tofazzal <its.emon@hotmail.com>
2026-03-16 06:37:05 -0400

    feat: add support for extra buttons in PageHeader component for enhanced customization

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -147,6 +147,7 @@ export function PageHeader({
                 Add
               </Button>
             )}
+            {extraButtons}
             {hasMobileDropdown && (
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
